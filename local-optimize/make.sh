11322295edb17192f67acdfe4e1aa094b84a5237
JARS3N <JARS3N@users.noreply.github.com>
2024-06-09 09:28:14 -0400

    Update make.sh

--- local-optimize/make.sh
+++ local-optimize/make.sh
@@ -1,9 +1,12 @@
-# Download source code
-wget http://ftp.gnu.org/gnu/make/make-<version>.tar.gz
-tar -xzf make-<version>.tar.gz
-cd make-<version>
+#!/bin/bash
+
+# Download the latest source code
+latest_version=$(wget -qO- ftp://ftp.gnu.org/gnu/make/ | grep -oP 'make-\K[0-9.]+(?=\.tar\.gz)' | sort -V | tail -1)
+wget ftp://ftp.gnu.org/gnu/make/make-$latest_version.tar.gz
+tar -xzf make-$latest_version.tar.gz
+cd make-$latest_version
 
 # Configure, compile, and install
-./configure --prefix=/usr/local/make-<version>
+./configure --prefix=/usr/local/make-$latest_version
 make -j$(nproc)
 sudo make install
