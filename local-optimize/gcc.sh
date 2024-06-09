4243be996d354ccb90d1d58a4bca76dce3c21f10
JARS3N <JARS3N@users.noreply.github.com>
2024-06-09 09:42:46 -0400

    Update gcc.sh

--- local-optimize/gcc.sh
+++ local-optimize/gcc.sh
@@ -1,17 +1,20 @@
-# Ensure prerequisites are installed
-sudo apt-get install build-essential
+#!/bin/bash
 
-# Download source code
-wget http://ftp.gnu.org/gnu/gcc/gcc-<version>/gcc-<version>.tar.gz
-tar -xzf gcc-<version>.tar.gz
-cd gcc-<version>
+# Ensure prerequisites are installed
+sudo apt-get install -y build-essential wget
+
+# Download the latest source code
+latest_version=$(wget -qO- ftp://ftp.gnu.org/gnu/gcc/ | grep -oP 'gcc-\K[0-9.]+(?=\/)' | sort -V | tail -1)
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-$latest_version/gcc-$latest_version.tar.gz
+tar -xzf gcc-$latest_version.tar.gz
+cd gcc-$latest_version
 
 # Install dependencies
 ./contrib/download_prerequisites
 
 # Create a build directory and configure
 mkdir build && cd build
-../configure --prefix=/usr/local/gcc-<version> --enable-languages=c,c++,fortran
+../configure --prefix=/usr/local/gcc-$latest_version --enable-languages=c,c++,fortran
 
 # Compile and install
 make -j$(nproc)
